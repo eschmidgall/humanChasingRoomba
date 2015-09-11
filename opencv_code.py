61f5c4fbe4e0495b3a4b04baf56f340a04d85a8a
Shachar Raindel <shacharr@gmail.com>
2015-09-11 15:56:25 +0300

    minor style cleanup

--- opencv_code.py
+++ opencv_code.py
@@ -10,7 +10,6 @@ import time
 VIDEO_URL="http://192.168.42.1:8080/?action=stream"
 
 
-stream=urllib.urlopen(VIDEO_URL)
 
 def detect(img, cascade):
     rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv2.CASCADE_SCALE_IMAGE)
@@ -34,30 +33,37 @@ def draw_rects(img, rects, color):
 def clock():
     return cv2.getTickCount() / cv2.getTickFrequency()
 
-bytes = ""
 
-for i in itertools.count(1):
-    bytes+=stream.read(1024)
-    a = bytes.find('\xff\xd8')
-    b = bytes.find('\xff\xd9')
-    if a!=-1 and b!=-1:
-        jpg = bytes[a:b+2]
-        bytes= bytes[b+2:]
-        img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.equalizeHist(gray)
-        t = clock()
-        rects = detect(gray, front_cascade)
+def handle_roomba():
+    stream=urllib.urlopen(VIDEO_URL)
 
-        if len(rects) == 0:
-            rects = detect(gray, profile_cascade)
+    bytes = ""
 
-        vis = img.copy()
-        draw_rects(vis, rects, (0, 255, 0))
-        dt = clock() - t
+    for i in itertools.count(1):
+        bytes+=stream.read(1024)
+        a = bytes.find('\xff\xd8')
+        b = bytes.find('\xff\xd9')
+        if a!=-1 and b!=-1:
+            jpg = bytes[a:b+2]
+            bytes= bytes[b+2:]
+            img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+            t = clock()
+            rects = detect(gray, front_cascade)
 
-        draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
-        cv2.imshow('facedetect', vis)
+            if len(rects) == 0:
+                rects = detect(gray, profile_cascade)
+
+            vis = img.copy()
+            draw_rects(vis, rects, (0, 255, 0))
+            dt = clock() - t
+
+            draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+            cv2.imshow('facedetect', vis)
         
-        if cv2.waitKey(1) == 27:
-            exit(0)
+            if cv2.waitKey(1) == 27:
+                break
+
+if __name__ == "__main__":
+    handle_roomba()
