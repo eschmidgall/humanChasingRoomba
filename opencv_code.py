34be6afe2499aa08bc21d5731abce5f15b790905
Shachar Raindel <shacharr@gmail.com>
2015-09-11 17:11:20 +0300

    Add meanshift tracker to the code

--- opencv_code.py
+++ opencv_code.py
@@ -58,10 +58,34 @@ class RoombaBrain(object):
         return rects
 
     def init_tracking(self,img,gray,rects):
-        pass
+
+        # set up the ROI for tracking
+        x1, y1, x2, y2 = rects[0]
+        r = x2
+        c = y2
+        w = x2 - x1
+        h = y2 - y1
+        c = max(c - h,0)
+        self.track_window = (c,r,w,h)
+        roi = img[r:r+h, c:c+w]
+        hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        self.roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+        cv2.normalize(self.roi_hist,self.roi_hist,0,255,cv2.NORM_MINMAX)
+        # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+        self.term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 
     def do_tracking(self,img,gray):
-        pass
+         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+         dst = cv2.calcBackProject([hsv],[0],self.roi_hist,[0,180],1)
+
+         # apply meanshift to get the new location
+         ret, self.track_window = cv2.meanShift(dst, self.track_window, self.term_crit)
+
+         # Draw it on image
+         x,y,w,h = self.track_window
+         img2 = cv2.rectangle(img, (x,y), (x+w,y+h), 255,2)
+         cv2.imshow('img2',img2)
 
     def handle_roomba(self):
         self.control_client = pyjsonrpc.HttpClient( url = CONTROL_URL)
